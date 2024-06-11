4a9e97804f3a751d356a23278eebf5540607de0b
tapendu-technoexponent98 <tapendu@technoexponent.com>
2024-06-11 11:20:22 +0530

    first commit

---
+++ lib/utils/AdConfigurationConstants.dart
@@ -0,0 +1,15 @@
+const disableAdMob = false;
+
+const String isGoogleAds="admob";
+const String isFacebookAds="facebook";
+
+const isAds=isGoogleAds;
+
+const mAdMobAppId = 'ADD AdMob AppId';
+const mAdMobBannerId = 'ADD AdMob BannerId';
+const mAdMobInterstitialId = 'ADD AdMob InterstitialId';
+const mAdMobNativeAdvancedId = 'ADD AdMob NativeAdvancedId';
+const mAdMobRewardedId = 'ADD AdMob RewardedId';
+
+String faceBookBannerPlacementId = "ADD YOUR FACEBOOK BANNER ID";
+String faceBookInterstitialPlacementId = "ADD YOUR FACE BOOK INTERTITIAL ID";
